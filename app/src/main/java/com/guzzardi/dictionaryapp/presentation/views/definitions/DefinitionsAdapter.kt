78e39d379c9e258a4fde1d1be6f1894f6bd35702
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2021-01-31 12:51:40 -0300

    added basic definition adapter implementation

--- app/src/main/java/com/guzzardi/dictionaryapp/presentation/views/definitions/DefinitionsAdapter.kt
+++ app/src/main/java/com/guzzardi/dictionaryapp/presentation/views/definitions/DefinitionsAdapter.kt
@@ -2,13 +2,17 @@ package com.guzzardi.dictionaryapp.presentation.views.definitions
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.guzzardi.dictionaryapp.data.DefinitionItemDto
 
-class DefinitionsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class DefinitionsAdapter(private val definitions: MutableList<DefinitionItemDto>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         DefinitionViewHolder.create(parent)
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val postItem = definitions[position]
+        (holder as DefinitionViewHolder).bind(postItem)
     }
 
     override fun getItemCount() = 0
